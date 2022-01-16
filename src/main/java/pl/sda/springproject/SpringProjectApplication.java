ca5a4c35af6168c1c4bcb3815571abe6e3b9870d
cs <siwon.cezary@gmail.com>
2022-01-16 10:25:04 +0100

    commandLineRunner - 1

--- src/main/java/pl/sda/springproject/SpringProjectApplication.java
+++ src/main/java/pl/sda/springproject/SpringProjectApplication.java
@@ -1,13 +1,25 @@
 package pl.sda.springproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class SpringProjectApplication {
+public class SpringProjectApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringProjectApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while(scanner.hasNextInt()){
+            sum += scanner.nextInt();
+        }
+        System.out.println("Suma: " + sum);
+    }
 }
